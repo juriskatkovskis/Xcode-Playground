feafd1068a20e70c26c8a4e6187621ddcc0a0412
juriskatkovskis <juris.katkovskis@accenture.com>
2022-07-25 13:52:56 +0300

    Lesson 3 homework

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -163,3 +163,55 @@ break
 
 
 }
+
+//Lesson 3
+/*
+ Exercise 1
+ The user opens a deposit in the bank for 5 years in the amount of 500_000 Eur. The interest rate per annum \(rate). Calculate the amount of income \(profit) at the end of the \(period).
+ for _ in 1...period{
+ }
+ print("Amount of income after \(period) years will be \(profit) Eur. My total deposit will be \(deposit) Eur !")
+ */
+
+let depositAtStart = 500_000
+let rate = 0.01 // 1% percent rate per year
+var depositWithRate = Double (depositAtStart) * Double (rate)
+    print("\(depositWithRate)")
+let period = [1, 2, 3, 4, 5]
+for _ in period{
+    // not finished
+    
+    
+    
+}
+
+
+/*
+ Exercise 2
+ Create an integer array with any set of numbers and  print("My even numbers are: \(evenNumber)")
+ Use a % inside the for loop.
+ */
+var arrayOfInt  = [Int]()
+arrayOfInt = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+let evenNumber =  arrayOfInt.filter {  $0 % 2 == 0 }
+print("My even numbers are: \(evenNumber)")
+
+/*
+ Exercise 3
+ Inside the for loop create randomNumber for the random Int calculation. Calculate and print("Number 5 will be after \(counter) shuffles"). Don't forget to make a break inside the if statement.
+ */
+//not sure about this
+
+var counter = Int.random(in: 0..<5)
+while true {
+    print("Number 5 will be after \(counter) shuffles.")
+    counter += 1
+    if counter == 5 {
+        break
+    }
+}
+
+/*
+ Exercise 4
+ A bug is climbing to a 10-meter electric post. During the day, bug can climb two meters, during the night she slides down to 1 meter. Determine with the help of the cycle how many days bug will climb on the top of the post. Think about which loop to use in which situation. print("bug will spend \(numberOfDays)) to reach top of the post")
+ */
