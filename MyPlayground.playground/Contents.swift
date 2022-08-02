09e7c938192aab89a2a3a8a3c919a14a13419a8b
juriskatkovskis <juris.katkovskis@accenture.com>
2022-08-02 16:29:36 +0300

    homework Lesson 6

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -370,10 +370,125 @@ Exercise 2
  6.Finally call myRectangle.description()
 */
 
+class Figure {
+    var height: Float
+    var width: Float
+    var radius: Float = 0
+    var square: Float = 0
+    var perimeter: Float = 0
+    
+    init(height:Float, width: Float){
+        self.height = height
+        self.width = width
+    }
+    func squareOfFigure() -> Float {
+        return square
+    }
+    func perimeterOfFigure() -> Float {
+        return perimeter
+    }
+}
+class Rectangle: Figure{
+    override func squareOfFigure() -> Float {
+        return height * width
+    }
+    override func perimeterOfFigure() -> Float {
+        return (height + width) * 2
+    }
+    func description() {
+        print("Rectangle area is \(squareOfFigure()), and perimeter of figure is: \(perimeterOfFigure())")
+    }
+}
+let myRectangle = Rectangle(height: 10, width: 10)
+myRectangle.description()
 
+// Lesson 6
+/*
+ Exercise 1
+ Create enum CalculationType as String, where will be: addition, subtraction, multiplication, division with String value
+ */
+enum CalculationType: String {
+    case addition = "+"
+    case substraction = "-"
+    case multiplication = "*"
+    case division = "/"
+}
 
-
+/*
+ Exercise 1.1
  
+ Create a func calculateResult with argument firstNumber, parametrs numberOne, data type Int, + argument andSecondNumber, parametrs numberTwo, data type Int ,+ argument withCalculationType, parametrs calculationType, data type CalculationType and everything will return Int.
+ 
+ Than use switch case to calculate and return Int result
+
+    var result = numberOne
+       
+       switch calculationType {
+       case .addition: result += numberTwo
+        .....
+       }
+  print("Result:  \(calculationType.rawValue) \(numberOne) and \(numberTwo) = \(result)")
+ 
+    return result
+ 
+ */
+
+func calculateResult(firstNumber numberOne: Int ,andSecondNumber numberTwo: Int, withCalculationType calculationType:CalculationType) -> Int{
+    var result = numberOne
+    switch calculationType{
+    case.addition: result += numberTwo
+    case.substraction: result -= numberTwo
+    case.multiplication: result *= numberTwo
+    case.division:
+        guard numberTwo > 0 else {
+            print("Can't divide by zero")
+            return result
+        }
+        
+    }
+    print("Result: \(calculationType.rawValue) of \(numberOne) and \(numberTwo) = \(result)")
+    return result
+}
+
+/*
+Exercise 1.2
+Declare two numbers.
+Call func 4 times for all calculateResult
+*/
+
+let firstNumber = 5
+let secondNumber = 0
+
+calculateResult(firstNumber: firstNumber, andSecondNumber: secondNumber, withCalculationType: .addition)
+calculateResult(firstNumber: firstNumber, andSecondNumber: secondNumber, withCalculationType: .substraction)
+calculateResult(firstNumber: firstNumber, andSecondNumber: secondNumber, withCalculationType: .multiplication)
+calculateResult(firstNumber: firstNumber, andSecondNumber: secondNumber, withCalculationType: .division)
+
+
+/*
+Exercise 2.1
+Make example of struct Car with the name audiQ7
+Make a copy of audiQ7 and name it audiTT.
+Provide the correct name for audiTT
+ 
+Than run getSpecs() for audiQ7 and audiTT
+ */
+
+struct Car {
+    var model: String?
+    var year: Int?
+    var color: String?
+    
+    func getSpecs(){
+        print("Model: \(model ?? "Not Available"), Year: \(year ?? 0), Color: \(color ?? "Not available")")
+    }
+}
+ let audiQ7 = Car(model: "Audi Q7", year: 2021, color: "White")
+var audiTT = audiQ7
+audiTT.model = "Audi TT"
+audiQ7.getSpecs()
+audiTT.getSpecs()
+
 
 
 
